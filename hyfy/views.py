a83ffae0d7444a24496533ddeb71a75d1b27b343
Stephen Scott <scott.stephen54@yahoo.co.uk>
2019-03-04 14:15:02 +0000

    views1

--- hyfy/views.py
+++ hyfy/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from datetime import datetime
 
 # Import the Category model
-from rango.models import Category
+from hyfy.models import Category
 
-from rango.models import Page
-from rango.forms import CategoryForm, PageForm
+from hyfy.models import Page
+from hyfy.forms import VenueForm
+
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
@@ -19,110 +20,142 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 
+
+
+def contact_us(request):
+
+    return render(request, 'rango/contact_us.html')
+
+
+def about_us(request):
+
+    return render(request, 'rango/about_us.html')
+
+
 def index(request):
-    request.session.set_test_cookie()
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
 
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-
-    response = render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html')
     return response
 
 
-def about(request):
-    if request.session.get('visits'):
-        count = request.session.get('visits')
-    else:
-        count = 0
-    # prints out whether the method is a GET or a POST
-    print(request.method)
-    # prints out the user name, if no one is logged in it prints `AnonymousUser`
-    print(request.user)
-    return render(request, 'rango/about.html', {'visits':count})
+def venue(request):
+
+    response = render(request, 'rango/venue.html')
+    return response
 
 
-def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
-    context_dict = {}
-
-    try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(slug=category_name_slug)
-
-        # Retrieve all of the associated pages.
-        # Note that filter() will return a list of page objects or an empty list
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything
-        # the template will display the "no category" message for us.
-        context_dict['category'] = None
-        context_dict['pages'] = None
-
-    # Go render the response and return it to the client.
-    return render(request, 'rango/category.html', context_dict)
-
-
-def add_category(request):
-    form = CategoryForm()
-
-    # A HTTP POST?
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-
-        # Have we been provided with a valid form?
-        if form.is_valid():
-                # Save the new category to the database.
-                form.save(commit=True)
-                # Now that the category is saved
-                # We could give a confirmation message
-                # But since the most recent category added is on the index page
-                # Then we can direct the user back to the index page.
-                return index(request)
-        else:
-                # The supplied form contained errors -
-                # just print them to the terminal.
-                print(form.errors)
-
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
-    return render(request, 'rango/add_category.html', {'form': form})
+# def show_venue(request, category_name_slug):
+#     # Create a context dictionary which we can pass
+#     # to the template rendering engine.
+#     context_dict = {}
+#
+#     try:
+#         # Can we find a category name slug with the given name?
+#         # If we can't, the .get() method raises a DoesNotExist exception.
+#         # So the .get() method returns one model instance or raises an exception.
+#         venue = Venue.objects.get(slug=venue_name_slug)
+#
+#         # Retrieve all of the associated pages.
+#         # Note that filter() will return a list of page objects or an empty list
+#         pages = Page.objects.filter(category=category)
+#
+#         # Adds our results list to the template context under name pages.
+#         context_dict['pages'] = pages
+#         # We also add the category object from
+#         # the database to the context dictionary.
+#         # We'll use this in the template to verify that the category exists.
+#         context_dict['category'] = category
+#     except Category.DoesNotExist:
+#         # We get here if we didn't find the specified category.
+#         # Don't do anything
+#         # the template will display the "no category" message for us.
+#         context_dict['category'] = None
+#         context_dict['pages'] = None
+#
+#     # Go render the response and return it to the client.
+#     return render(request, 'rango/category.html', context_dict)
 
 
-def add_page(request, category_name_slug):
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
-        category = None
 
-    form = PageForm()
-    if request.method == 'POST':
-        form = PageForm(request.POST)
-        if form.is_valid():
-            if category:
-                page = form.save(commit=False)
-                page.category = category
-                page.views = 0
-                page.save()
-                # probably better to use a redirect here.
-            return show_category(request, category_name_slug)
-        else:
-            print(form.errors)
-    context_dict = {'form':form, 'category': category}
-    return render(request, 'rango/add_page.html', context_dict)
+# def show_category(request, category_name_slug):
+#     # Create a context dictionary which we can pass
+#     # to the template rendering engine.
+#     context_dict = {}
+#
+#     try:
+#         # Can we find a category name slug with the given name?
+#         # If we can't, the .get() method raises a DoesNotExist exception.
+#         # So the .get() method returns one model instance or raises an exception.
+#         category = Category.objects.get(slug=category_name_slug)
+#
+#         # Retrieve all of the associated pages.
+#         # Note that filter() will return a list of page objects or an empty list
+#         pages = Page.objects.filter(category=category)
+#
+#         # Adds our results list to the template context under name pages.
+#         context_dict['pages'] = pages
+#         # We also add the category object from
+#         # the database to the context dictionary.
+#         # We'll use this in the template to verify that the category exists.
+#         context_dict['category'] = category
+#     except Category.DoesNotExist:
+#         # We get here if we didn't find the specified category.
+#         # Don't do anything
+#         # the template will display the "no category" message for us.
+#         context_dict['category'] = None
+#         context_dict['pages'] = None
+#
+#     # Go render the response and return it to the client.
+#     return render(request, 'rango/category.html', context_dict)
+
+
+# def add_category(request):
+#     form = CategoryForm()
+#
+#     # A HTTP POST?
+#     if request.method == 'POST':
+#         form = CategoryForm(request.POST)
+#
+#         # Have we been provided with a valid form?
+#         if form.is_valid():
+#                 # Save the new category to the database.
+#                 form.save(commit=True)
+#                 # Now that the category is saved
+#                 # We could give a confirmation message
+#                 # But since the most recent category added is on the index page
+#                 # Then we can direct the user back to the index page.
+#                 return index(request)
+#         else:
+#                 # The supplied form contained errors -
+#                 # just print them to the terminal.
+#                 print(form.errors)
+#
+#     # Will handle the bad form, new form, or no form supplied cases.
+#     # Render the form with error messages (if any).
+#     return render(request, 'rango/add_category.html', {'form': form})
+
+
+# def add_page(request, category_name_slug):
+#     try:
+#         category = Category.objects.get(slug=category_name_slug)
+#     except Category.DoesNotExist:
+#         category = None
+#
+#     form = PageForm()
+#     if request.method == 'POST':
+#         form = PageForm(request.POST)
+#         if form.is_valid():
+#             if category:
+#                 page = form.save(commit=False)
+#                 page.category = category
+#                 page.views = 0
+#                 page.save()
+#                 # probably better to use a redirect here.
+#             return show_category(request, category_name_slug)
+#         else:
+#             print(form.errors)
+#     context_dict = {'form':form, 'category': category}
+#     return render(request, 'rango/add_page.html', context_dict)
 
 #
 # def register(request):
@@ -228,9 +261,9 @@ def add_page(request, category_name_slug):
 #         return render(request, 'rango/login.html', {})
 
 
-@login_required
-def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+# @login_required
+# def restricted(request):
+#     return HttpResponse("Since you're logged in, you can see this text!")
 #
 #
 # # Use the login_required() decorator to ensure only those logged in can
@@ -241,33 +274,3 @@ def restricted(request):
 #     logout(request)
 #     # Take the user back to the homepage.
 #     return HttpResponseRedirect(reverse('index'))
-
-
-# A helper method
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-# Updated the function definition
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,
-                                                'last_visit',
-                                                str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        # update the last visit cookie now that we have updated the count
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        # set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie
-
-        # Update/set the visits cookie
-        request.session['visits'] = visits
