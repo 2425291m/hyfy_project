9b50271050df75788449a754386d5314db653f36
euan macqueen <2425291m@student.gla.ac.uk>
2019-03-06 11:18:20 +0000

    deleted hyfy_project attempt

--- hyfy_project/hyfy/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from hyfy.models import City
-
-# Register your models here.
-class CityAdmin (admin.ModelAdmin):
-    list_display = ('name',)
-
-admin.site.register(City, CityAdmin)
