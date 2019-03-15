27e0dcc3cfb8f16eb83dd8245d84e1e76d1d95f2
Ollie Musgrove <2426628R@student.gla.ac.uk>
2019-03-15 10:55:24 +0000

    hyfy-venue.js has ajax troubles

--- static/js/hyfy-venue.js
+++ static/js/hyfy-venue.js
@@ -1,17 +1,21 @@
-$('#reviewForm').submit(function(event) {
+<script type="text/javascript">
+var frm = '#review_form';
+
+frm.submit(function(event) {
     alert("You made a review!");
     event.preventDefault();
-    });
     $.ajax({
         type: "POST",
-        url:"/hyfy/post_comment/{{ venue.slug }}/",
+        url:"/hyfy/show_review/{{ venue.slug }}/",
         data:{
-            'csrfmiddlewaretoken': $('input[name=csrf_token]').val()
+            'csrfmiddlewaretoken': $('input[name=csrf_token]').val(),
+            'review': frm.serialize(),
         },
         success: function(data) {
-            $('#commentlist').append(data);
+            $('#review-user').children().append(data);
+            $('#review-text').children().append(data);
         },
         dataType: 'html'
     });
 });
-
+</script>
