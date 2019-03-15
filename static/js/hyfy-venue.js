c31986e15ac8239ca0af01acc0b95a6660a10727
Ollie Musgrove <2426628R@student.gla.ac.uk>
2019-03-15 17:50:49 +0000

    ajax integrated

--- static/js/hyfy-venue.js
+++ static/js/hyfy-venue.js
@@ -1,17 +1,22 @@
-$('#reviewForm').submit(function(event) {
-    alert("You made a review!");
-    event.preventDefault();
-    });
-    $.ajax({
-        type: "POST",
-        url:"/hyfy/post_comment/{{ venue.slug }}/",
-        data:{
-            'csrfmiddlewaretoken': $('input[name=csrf_token]').val()
-        },
-        success: function(data) {
-            $('#commentlist').append(data);
-        },
-        dataType: 'html'
-    });
-});
+//var frm = '#review_form';
 
+$(document).ready(function() {
+
+    $('#review_form').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url:"/hyfy/add_review/",
+            data:{
+                'csrfmiddlewaretoken': $('#review_form input[name=csrfmiddlewaretoken]').val(),
+                'venue_slug': $('#review_form input[name=venue_slug]').val(),
+                'comment': $('#comment_text').val(),
+            },
+            success: function(data) {
+                $('#review-display').html(data);
+            },
+            dataType: 'html'
+        });
+    });
+
+});
